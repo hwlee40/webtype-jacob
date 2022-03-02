f84c18f60405daae710e236f42924df43401d0bb
Hwan (Jacob) Lee <jacobhwanlee17@gmail.com>
2022-03-01 20:26:45 -0500

    Project 1 Draft 1
    Developed for 14inch screen size for now

---
+++ project1/main.js
@@ -0,0 +1,12 @@
+function translateSpine() {
+    document.getElementById("spine_title").style.transform = "translateY(30px)"
+}
+function translateSpineUp() {
+    document.getElementById("spine_title").style.transform = "translateY(0)"
+}
+function translateSpine2() {
+    document.getElementById("spine_title2").style.transform = "translateY(30px)"
+}
+function translateSpineUp2() {
+    document.getElementById("spine_title2").style.transform = "translateY(0)"
+}
