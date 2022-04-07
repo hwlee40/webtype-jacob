4c434d79b7757d34709960e82fa1f04b0977bc34
Hwan (Jacob) Lee <jacobhwanlee17@gmail.com>
2022-04-07 15:40:48 -0400

    input range exercise

--- script.js
+++ script.js
@@ -1,8 +1,25 @@
 $(document).ready(function() {
+
     $('#darkmode').click(function(){
         console.log("hello world!");
 
         $('body').toggleClass('darkmode');
         $('a').toggleClass('darkmode');
     });
+    $('#animate-title').click(function(){
+        $('.xno').toggleClass('animate');
+    })
+
+    $('input[type=range]').on('input', function(){
+        let inputType = $(this).attr('id');
+        let inputValue = $(this).val();
+        // console.log($(this).attr('id'));
+        if (inputType === 'opsz'){
+            $('#slider').css('--opsz-axis', inputValue);
+        } else {
+            $('#slider').css('--wght-axis', inputValue);
+        }
+    })
+
 });
+
