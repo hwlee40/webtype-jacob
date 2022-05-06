e47b3bee32539ea0d9273a0bfcf27e826d2dc88e
Hwan (Jacob) Lee <jacobhwanlee17@gmail.com>
2022-05-06 18:34:06 -0400

    project 4 all content added

--- project4/movies/script.js
+++ project4/movies/script.js
@@ -1,37 +1,62 @@
 $(document).ready(function(){
 // Document Load
 
-    let movieTitle = "tt0470752"
 
-    $.getJSON("https://www.omdbapi.com/?apikey=140382f9&i=" + movieTitle)
+    let key = $('body').attr('key');
+    let number = $('body').attr('number');
+
+    $.getJSON("https://www.omdbapi.com/?apikey=140382f9&i=" + key)
         .done(function(data) {
-            console.log(data);
+            // console.log(data);
 
-            $('.year0').append('<div>' + data['Released'] + '</div>')
-            $('.genre0').append('<div>' + data['Genre'] + '</div>')
-            $('.plot0').append('<div>' + data['Plot'] + '</div>')
-            $('.runtime0').append('<div>' + data['Runtime'] + '</div>')
-            $('.director0').append('<div>' + data['Director'] + '</div>')
+            $('.year').append('<div>' + data['Released'] + '</div>')
+            $('.genre').append('<div>' + data['Genre'] + '</div>')
+            $('.plot').append('<div>' + data['Plot'] + '</div>')
+            $('.runtime').append('<div>' + data['Runtime'] + '</div>')
+            $('.director').append('<div>' + data['Director'] + '</div>')
             
 
         });
+    
+    let info;
 
     $.getJSON("../movielist.json")
     .done(function(data) {
+
+        
+        // List
         for (let i = 0; i<data.length; i++) {
             $('.list0').append(
                 '<a href="movie' + i + '.html" class="movie-button">' + data[i].Title +'</a>')
-        }
+        };
 
-        $('.title0').append('<div>' + data[0].Title + '</div>')
+        for(let i = 0; i < data.length; i++){
+            
+            let item = data[i];
+            // console.log (item['Number']);
+            // console.log(number);
+            if (item['Number'] == number){
+                info = item;
+            };
+        };
 
-        $('.comments0').append('<div>' + data[0].Comments + '</div>')
-            $('.watched0').append('<div>' + data[0].Watched + '</div>')
+        // console.log(info);
+
+        //Page Title
+        $('.title0').append('<div>' + info['Title'] + '</div>')
+
+        $('head title').text(info['Title']);
+
+        // Comments and Watched
+        $('.comments').append('<div>' + info['Comments'] + '</div>')
+        $('.watched').append('<div>' + info['Watched'] + '</div>')
         
 
-
     });
+    
 
+    //CHANGE DATE HERE TO UPDATE ON ALL PAGES
+    $('.replace-date').append('<span>May 6, 2022</span>')
 
 // Document Load    
 });
